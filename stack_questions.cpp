5133bd5c6e3616a7a8f546da924cc6d01a90af58
Varun Sirpal <varunsirpal5@gmail.com>
2020-04-07 07:45:12 +0530

    stack

--- stack_questions.cpp
+++ stack_questions.cpp
@@ -59,15 +59,91 @@ bool duplicateParenthesis(string str){
 	return false;
 }
 
+bool isDuplicate(string str){
+
+	stack<char> s;
+
+	for(int i=0;i<str.length();i++){
+
+		char ch = str[i];
+		if(ch != ')'){
+			s.push(ch);
+		}
+
+		else{
+			if(s.top() == '('){
+				return true;
+			}
+
+			while(s.top() != '('){
+				s.pop();
+			}
+			s.pop();
+		}
+	}
+	return false;
+}
+
+void nextGreater(int arr[], int n){
+
+	stack<int> s;
+
+	for(int i=0;i<n;i++){
+
+		while(!s.empty() and s.top()<arr[i]){
+			int temp = s.top();
+			s.pop();
+			cout<<temp<<" -> "<<arr[i]<<endl;
+		}
+
+		s.push(arr[i]);
+	}
+
+	while(!s.empty()){
+		int temp = s.top();
+		s.pop();
+		cout<<temp<<" -> "<<-1<<endl;
+	}
+}
+
+void stockSpanProblem(int arr[], int n){
+
+	stack<int> s;
+
+	for(int i=0;i<n;i++){
+
+		while(!s.empty() and arr[s.top()] <= arr[i]){
+			s.pop();
+		}
+
+		if(s.empty()){
+			cout<<arr[i]<<" -> "<<i+1<<endl;
+		}
+		
+		else{
+			int idx = s.top();
+			cout<<arr[i]<<" -> "<<(i - s.top())<<endl;
+		}
+
+		s.push(i);
+	}
+}
+
 int main(){
 
 	// string str = ")))(((";
 	// cout<<balancedParanthesis(str)<<endl;
 
-	string str = "((a+b)+((c+d)))";
+	// string str = "((a+b)+((c+d)))";
 	// string str = "((a+(b))+(c+d))";
-	cout<<duplicateParenthesis(str)<<endl;
+	// cout<<duplicateParenthesis(str)<<endl;
 
+	int arr[] = {100,80,60,70,60,75,85};
+	int n = 7;
+	stockSpanProblem(arr,n);	
+
+	// int arr[] = {59,8,7,12,2,14};
+	// nextGreater(arr,6);
 
 	return 0;
 }
