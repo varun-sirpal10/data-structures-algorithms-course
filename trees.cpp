821f693b7f2eb7181b3aeedb9d8f3b856cfea54e
Varun Sirpal <varunsirpal5@gmail.com>
2020-04-17 08:54:06 +0530

    tres

--- trees.cpp
+++ trees.cpp
@@ -246,6 +246,47 @@ void levelOrderNewLine(TreeNode* root){
 		}
 	}
 }
+    
+vector<int> rightViewOfTree(TreeNode* root, vector<int> &result){
+    
+    if(root == NULL){
+        return result;
+    }
+    
+    queue<TreeNode*> q;
+    
+    q.push(root);
+    
+    while(!q.empty()){
+        
+        int len = q.size();
+        
+        for(int i=0;i<len;i++){
+            
+            TreeNode* temp = q.front();
+            q.pop();
+            
+            if(i == len-1){
+                result.push_back(temp->val);
+            }
+            
+            if(temp->left != NULL){
+                q.push(temp->left);
+            }
+            
+            if(temp->right != NULL){
+                q.push(temp->right);
+            }
+        }
+    }
+    
+    return result;
+}
+
+vector<int> rightSideView(TreeNode* root) {
+    vector<int> result;
+    return rightViewOfTree(root,result);
+}
 
 int main() {
 
