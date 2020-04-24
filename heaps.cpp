342a0faade8ec422af0729f2275992ac5767dbe8
Varun Sirpal <varunsirpal5@gmail.com>
2020-04-24 13:30:04 +0530

    heaps

--- heaps.cpp
+++ heaps.cpp
@@ -20,6 +20,30 @@ void upHeapify(vector<int> &heap, int idx){
 	}
 }
 
+void downHeapify(vector<int> &heap, int idx){
+	int leftIdx = 2*idx + 1;
+	int rightIdx = 2*idx + 2;
+	if(leftIdx >= heap.size() and rightIdx >= heap.size()){
+		return;
+	}
+	int largestIdx = idx;
+	if(leftIdx < heap.size() and heap[leftIdx] > heap[largestIdx]){
+		largestIdx = leftIdx;
+	}
+	if(rightIdx < heap.size() and heap[rightIdx] > heap[largestIdx]){
+		largestIdx = rightIdx;
+	}
+	if(largestIdx == idx) return;
+	swap(heap[idx],heap[largestIdx]);
+	downHeapify(heap,largestIdx);
+}
+
+void deletePeakElement(vector<int> &heap){
+	swap(heap[0],heap[heap.size() - 1]);
+	heap.pop_back();
+	downHeapify(heap,0);
+}
+
 void insert(vector<int> &heap, int key){
 	heap.push_back(key);
 	upHeapify(heap,heap.size()-1);
@@ -43,6 +67,8 @@ int main(){
 		cin>>x;
 		insert(heap,x);
 	}
+	display(heap);
+	deletePeakElement(heap);
 	display(heap);
 	return 0;
 }
