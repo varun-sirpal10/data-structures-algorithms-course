04b1f97f474c776fc7a734997f7879a0ec219fa8
Varun Sirpal <varunsirpal5@gmail.com>
2020-04-25 19:56:41 +0530

    heaps

---
+++ comparator.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+class Compare {
+public:
+	bool operator()(int a, int b) {
+		return a < b;
+
+		// a < b -> Max Heap
+		// a > b -> Min Heap
+	}
+};
+
+int main() {
+
+	// Compare p;
+	// cout << p(1, 2) << endl;
+
+	priority_queue<int, vector<int>, Compare> pq;
+
+	// Compare c;
+	// c(45, 15);
+
+	pq.push(45);
+	pq.push(5);
+	pq.push(43);
+	pq.push(15);
+	pq.push(89);
+
+	while (!pq.empty()) {
+		cout << pq.top() << endl;
+		pq.pop();
+	}
+
+	return 0;
+}
