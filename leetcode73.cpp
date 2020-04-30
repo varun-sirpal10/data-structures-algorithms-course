63f06a10f5a4db5f945c24982d702a262be6e04f
Varun Sirpal <varunsirpal5@gmail.com>
2020-04-30 16:22:56 +0530

    2D array

---
+++ leetcode73.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        
+        int m = matrix.size();
+        int n = matrix[0].size();
+        bool is_col = false;
+        
+        for(int i=0;i<m;i++){
+            if(matrix[i][0] == 0){
+                is_col = true;
+            }
+            
+            for(int j=1;j<n;j++){
+                if(matrix[i][j] == 0){
+                    matrix[0][j] = 0;
+                    matrix[i][0] = 0;
+                }
+            }
+        }
+        
+        for(int i=1;i<m;i++){
+            for(int j=1;j<n;j++){
+                if(matrix[0][j] == 0 or matrix[i][0] == 0){
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+        
+        if(matrix[0][0] == 0){
+            for(int j=1;j<n;j++){
+                matrix[0][j] = 0;
+            }
+        }
+        
+        if(is_col){
+             for(int i=0;i<m;i++){
+                matrix[i][0] = 0;
+            }   
+        }
+    }
+};
