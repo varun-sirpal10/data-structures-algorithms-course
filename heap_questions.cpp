37c1a4cfdf91411315975bb346b01d30c269767a
Varun Sirpal <varunsirpal5@gmail.com>
2020-04-30 16:22:27 +0530

    heaps

--- heap_questions.cpp
+++ heap_questions.cpp
@@ -117,6 +117,40 @@ vector<int> maxSlidingWindow(vector<int>& nums, int k) {
     return res;
 }
 
+class MedianFinder {
+public:
+    priority_queue<int> maxHeap;
+    priority_queue<int, vector<int>, greater<int> > minHeap;
+    
+    /** initialize your data structure here. */
+    MedianFinder() {}
+    
+    void addNum(int num) {
+        if(maxHeap.empty() or num < maxHeap.top()){
+            maxHeap.push(num);
+        }
+        else{
+            minHeap.push(num);
+        }
+        
+        if(maxHeap.size() > minHeap.size() + 1){
+            minHeap.push(maxHeap.top());
+            maxHeap.pop();
+        }
+        else if(minHeap.size() > maxHeap.size() + 1){
+            maxHeap.push(minHeap.top());
+            minHeap.pop();
+        }
+    }
+    
+    double findMedian() {
+        if(maxHeap.size()==minHeap.size()){
+            if(maxHeap.empty()){ return 0; }
+            return (maxHeap.top()+minHeap.top())/2.0;
+        }
+        return maxHeap.size() > minHeap.size() ? maxHeap.top() : minHeap.top();
+    }
+};
 
 
 int main(){
