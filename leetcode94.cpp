70f538fcd6438b767ff207ffac0c19193f8769dc
Varun Sirpal <varunsirpal5@gmail.com>
2020-05-02 11:38:36 +0530

    trees

---
+++ leetcode94.cpp
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        
+        vector<int> res;
+        
+        return helper(root,res);
+    }
+    
+    vector<int> helper(TreeNode* root,vector<int> &res){
+        
+        if(root==NULL){
+            return res;
+        }
+        
+        helper(root->left,res);
+        
+        res.push_back(root->val);
+        
+        helper(root->right,res);
+        
+        return res;
+    }
+};
