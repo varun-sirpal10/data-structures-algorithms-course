a649e879c505dd332c798e587c05ad73fce01e56
Varun Sirpal <varunsirpal5@gmail.com>
2020-05-06 07:53:22 +0530

    hashmap

--- hashmap_questions.cpp
+++ hashmap_questions.cpp
@@ -129,6 +129,46 @@ int lengthOfLongestSubstring(string s) {
     return maxLen;
 }
 
+vector<string> topKFrequent(vector<string>& words, int k) {
+        
+    unordered_map<string,int> freq;
+    
+    for(auto word:words){
+        freq[word]++;
+    }
+    
+    class Compare{
+    public:
+        bool operator()(pair<int,string> val1, pair<int,string> val2){
+            if(val1.first == val2.first){
+                return val1.second < val2.second;
+            }
+            
+            return val1.first > val2.first;
+        }
+    };
+    
+    priority_queue<pair<int,string>, vector<pair<int,string>>, Compare > pq;
+    
+    for(auto val:freq){
+        pq.push({val.second,val.first});
+    }
+    
+    while(pq.size() > k){
+        pq.pop();
+    }
+    
+    vector<string> res;
+    
+    while(!pq.empty()){
+        res.push_back(pq.top().second);
+        pq.pop();
+    }
+    
+    reverse(res.begin(),res.end());
+    return res;
+}
+
 int main(){
 
 	// int arr[] = {1,2,3,-3,4,5,4};
