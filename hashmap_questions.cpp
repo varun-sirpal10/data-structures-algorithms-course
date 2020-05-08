4d88d4bd59d6c85c8553e8a2750a570419040fc7
Varun Sirpal <varunsirpal5@gmail.com>
2020-05-08 11:08:10 +0530

    hashmap

--- hashmap_questions.cpp
+++ hashmap_questions.cpp
@@ -190,6 +190,37 @@ int subarraySum(vector<int>& nums, int k) {
     return count;
 }	
 
+int longestConsecutive(vector<int>& nums) {
+        
+    unordered_map<int,bool> hp;
+    
+    for(auto num:nums){
+        hp[num] = true;
+    }
+    
+    int n = nums.size();
+    
+    int ans = 0;
+    
+    for(int i=0;i<n;i++){
+        
+        if(hp.count(nums[i] - 1)){
+            continue;
+        }
+        else{ // LEFT BOUNDARY
+            int count = 0;
+            int x = nums[i];
+            while(hp.count(x)){
+                count++;
+                x++;
+            }
+            ans = max(ans,count);
+        }
+    }
+    
+    return ans;
+}
+
 int main(){
 
 	// int arr[] = {1,2,3,-3,4,5,4};
