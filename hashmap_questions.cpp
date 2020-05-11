ebbf1fe03d41272b84d90c3932fe1149cd2e5399
Varun Sirpal <varunsirpal5@gmail.com>
2020-05-11 12:52:35 +0530

    hashmap

--- hashmap_questions.cpp
+++ hashmap_questions.cpp
@@ -305,6 +305,30 @@ string minWindow(string s, string t) {
     return ans;
 }
 
+int findShortestSubArray(vector<int>& nums) {
+    unordered_map<int,int> freq;
+    unordered_map<int,int> first_seen;
+    
+    int degree = 0;
+    int minLen = 0;
+    
+    for(int i=0;i<nums.size();i++){
+        freq[nums[i]]++;
+        if(!first_seen.count(nums[i])){
+            first_seen[nums[i]] = i;
+        }
+        if(freq[nums[i]] > degree){
+            degree = freq[nums[i]];
+            minLen = i - first_seen[nums[i]] + 1;
+        }
+        else if(freq[nums[i]] == degree){
+            minLen = min(minLen,i - first_seen[nums[i]] + 1);
+        }
+    }
+    
+    return minLen;
+}
+
 int main(){
 
 	// int arr[] = {1,2,3,-3,4,5,4};
