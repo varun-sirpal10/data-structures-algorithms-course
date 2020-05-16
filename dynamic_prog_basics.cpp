811bbddf6a8dcb57f894162a08a05d65235e034e
Varun Sirpal <varunsirpal5@gmail.com>
2020-05-16 19:56:56 +0530

    dp

--- dynamic_prog_basics.cpp
+++ dynamic_prog_basics.cpp
@@ -279,6 +279,52 @@ int numTreesPureDP(int n) {
     return dp[n];
 }
 
+int minCostClimbingStairs(vector<int>& cost) {
+        
+    int n = cost.size();
+    int dp[n];
+    
+    dp[0] = cost[0];
+    dp[1] = cost[1];
+    
+    for(int i=2;i<n;i++){
+        dp[i] = cost[i] + min(dp[i-1],dp[i-2]);
+    }
+    
+    return min(dp[n-1],dp[n-2]);
+}
+
+int minPathSum(vector<vector<int>>& grid) {
+    if(grid.size() == 0){
+        return 0;
+    }
+    
+    int m = grid.size();
+    int n = grid[0].size();
+    
+    vector<vector<int>> dp(m, vector<int> (n, 0));
+    
+    for(int i=0;i<m;i++){
+        
+        for(int j=0;j<n;j++){
+            dp[i][j] += grid[i][j];
+            
+            if(i>0 and j>0){
+                dp[i][j] += min(dp[i-1][j],dp[i][j-1]);
+            }
+            else if(i>0){
+                dp[i][j] += dp[i-1][j];
+            }
+            else if(j>0){
+                dp[i][j] += dp[i][j-1];
+            }
+        }
+    }
+    
+    return dp[m-1][n-1];
+}
+
+
 
 
 int main(){
