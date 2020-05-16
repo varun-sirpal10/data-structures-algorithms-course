01dd3018d2f1f6fdf01c7ab35cf31a6e1763f0ed
Varun Sirpal <varunsirpal5@gmail.com>
2020-05-16 08:44:39 +0530

    dp

--- dynamic_prog_basics.cpp
+++ dynamic_prog_basics.cpp
@@ -255,6 +255,32 @@ int maxSubArray(vector<int>& nums) {
     return maxSum;
 }
 
+int numTreesPureDP(int n) {
+        
+    int dp[n+1];
+    
+    dp[0] = 1;
+    dp[1] = 1;
+    
+    for(int i=2;i<=n;i++){
+        
+        int ans = 0;
+        
+        for(int j=1;j<=i;j++){
+            int leftCount = dp[j - 1];
+            int rightCount = dp[i - j];
+
+            ans += leftCount*rightCount;    
+        }
+        
+        dp[i] = ans;
+    }
+    
+    return dp[n];
+}
+
+
+
 int main(){
 
 	// int n = 5;
