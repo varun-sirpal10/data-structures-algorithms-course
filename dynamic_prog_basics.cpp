2d74d89ca276acbeab73996f48025bed2b82ea31
Varun Sirpal <varunsirpal5@gmail.com>
2020-05-17 09:48:27 +0530

    dp

--- dynamic_prog_basics.cpp
+++ dynamic_prog_basics.cpp
@@ -324,8 +324,28 @@ int minPathSum(vector<vector<int>>& grid) {
     return dp[m-1][n-1];
 }
 
-
-
+int lengthOfLIS(vector<int>& nums) {
+    int n = nums.size();
+    
+    if(n == 0){
+        return 0;
+    }
+    
+    vector<int> dp(n,1);
+    
+    int maxValue = 1;
+    
+    for(int i=1;i<n;i++){
+        for(int j=0;j<i;j++){
+            if(nums[i] > nums[j]){
+                dp[i] = max(dp[i],dp[j]+1);
+            }
+        }
+        maxValue = max(maxValue,dp[i]);
+    }
+    
+    return maxValue;
+}
 
 int main(){
 
