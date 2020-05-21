6f3437d81a51c21f4c445300ff1913101205617f
Varun Sirpal <varunsirpal5@gmail.com>
2020-05-21 10:11:06 +0530

    dp

--- dynamic_prog_basics.cpp
+++ dynamic_prog_basics.cpp
@@ -525,6 +525,60 @@ int maxEnvelopes(vector<vector<int>>& envelopes) {
     return *max_element(dp.begin(),dp.end());
 }
 
+int minFallingPathSum(vector<vector<int>>& A) {
+    int m = A.size();
+
+    vector<vector<int>>dp(m, vector<int>(m, 0));
+    
+    for (int i = 0; i < m; i++)
+        dp[0][i] = A[0][i];
+    
+    for (int i = 1; i < m; i++) {
+        for (int j = 0; j < m; j++) {
+            if (j == 0)
+                dp[i][j] = A[i][j] + min(dp[i-1][j], dp[i-1][j+1]);
+            else if (j == m-1)
+                dp[i][j] = A[i][j] + min(dp[i-1][j], dp[i-1][j-1]);
+            else
+                dp[i][j] = A[i][j] + min(dp[i-1][j], min(dp[i-1][j-1], dp[i-1][j+1]));
+        }
+    }
+    
+    return *min_element(dp[m-1].begin(),dp[m-1].end());
+}
+
+int knapSackPureDP(int weight[], int value[], int capacity, int n){
+    vector<vector<int>> dp(n+1,vector<int> (capacity+1,0));
+
+    //BASE CASE
+    for(int i=0;i<=n;i++){
+        dp[i][0] = 0;
+    }
+
+    for(int j=0;j<=capacity;j++){
+        dp[0][j] = 0;
+    }
+
+    //RECURSIVE CASE
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=capacity;j++){
+
+            int include = INT_MIN;
+            int exclude = INT_MIN;
+
+            if(j >= weight[i - 1]){
+                include = value[i - 1] + dp[i - 1][j - weight[i - 1]];
+            }
+
+            exclude = dp[i-1][j];
+
+            dp[i][j] = max(include,exclude);
+        }
+    }
+
+    return dp[n][capacity];
+}
+
 int main(){
 
 	// int n = 5;
@@ -552,8 +606,14 @@ int main(){
 	// cout<<countBoardPathPureDP(0,10)<<endl;
 
 
-	cout<<longestPalindromeSubseqPureDP("bbbab")<<endl;
+	// cout<<longestPalindromeSubseqPureDP("bbbab")<<endl;
 
+    int value[] = {50, 40, 70, 40};
+    int weight[] = {5, 4, 6, 3};
+    int capacity = 8;
+    int n = 4;
+
+    cout<<knapSackPureDP(weight,value,capacity,n)<<endl;
 
 	return 0;
 }
