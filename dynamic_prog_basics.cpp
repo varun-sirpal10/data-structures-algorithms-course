a1c4b32cd1cbb457bc760d52548eff8c44e2cf7c
Varun Sirpal <varunsirpal5@gmail.com>
2020-05-23 20:16:55 +0530

    dp

--- dynamic_prog_basics.cpp
+++ dynamic_prog_basics.cpp
@@ -579,6 +579,142 @@ int knapSackPureDP(int weight[], int value[], int capacity, int n){
     return dp[n][capacity];
 }
 
+int CoinChange2Memo(int amount, vector<int>& coins) {
+    int n = coins.size();
+    
+    if(amount == 0){
+        return 1;
+    }
+    
+    if(n == 0){
+        return 0;
+    }
+    
+    vector<vector<int>> dp(n+1, vector<int> (amount+1,-1));
+    
+    return helper(0,amount,coins,dp);
+}
+
+int helper(int si, int amount, vector<int> &coins, vector<vector<int>> &dp){
+    if(si == coins.size()){
+        return 0;
+    }
+    
+    if(amount == 0){
+        return 1;
+    }
+    
+    
+    if(dp[si][amount] != -1){
+        return dp[si][amount];
+    }
+    
+    //RECURSIVE CASE
+    int count = 0;
+    
+    if(amount >= coins[si]){
+        count += helper(si,amount - coins[si], coins, dp);
+    }
+    
+    count += helper(si+1,amount,coins,dp);
+    
+    dp[si][amount] = count;
+    
+    return count;
+}
+
+int CoinChangePureDP(int amount, vector<int>& coins) {
+    
+    if(amount == 0){
+        return 1;
+    }
+    
+    if(coins.size() == 0){
+        return 0;
+    }
+    
+    int row = coins.size() + 1;
+    int col = amount + 1;
+    
+    vector<vector<int>> dp(row,vector<int> (col,0));
+    
+    //BASE CASE
+    for(int j=0;j<=amount;j++){
+        dp[coins.size()][j] = 0;
+    }
+    
+    for(int i=0;i<=coins.size();i++){
+        dp[i][0] = 1;
+    }
+    
+    //RECURSIVE CASE
+    for(int j=1;j<=amount;j++){
+        for(int i=coins.size()-1;i>=0;i--){
+            
+            if(j >= coins[i]){
+                dp[i][j] += dp[i][j-coins[i]];
+            }
+            
+            dp[i][j] += dp[i+1][j];                
+        }
+    }
+    
+    return dp[0][amount];
+}
+
+int coinChange(vector<int>& coins, int amount) {
+        
+    vector<int> dp(amount+1,0);
+    
+    //BASE CASE
+    dp[0] = 0;
+    
+    
+    //RECURSIVE CASE
+    for(int i=1;i<=amount;i++){
+        
+        int count = amount+1;
+        
+        for(int j=0;j<coins.size();j++){
+
+            if(coins[j] <= i){
+                count = min(count,dp[i-coins[j]] + 1);   
+            }
+        }
+        
+        dp[i] = count;
+    }
+    
+    return dp[amount] > amount ? -1 : dp[amount];
+}
+
+int combinationSum4(vector<int>& nums, int target) {
+        
+    if(nums.size() == 0){
+        return 0;
+    }
+    
+    vector<unsigned int> dp(target + 1,0);
+    
+    sort(nums.begin(),nums.end());
+    
+    dp[0] = 1;
+    
+    for(int i=1;i<=target;i++){
+        
+        for(int j=0;j<nums.size();j++){
+            
+            if(i < nums[j]) break;
+            
+            dp[i] += dp[i - nums[j]]; 
+        }
+    }
+    
+    return dp[target];
+}
+
+
+
 int main(){
 
 	// int n = 5;
